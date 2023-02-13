b78522ffafdfa6502f336b542de4e7777d0c7542
Recep Gultekin <recep.gultekin@inetum-realdolmen.world>
2023-02-13 12:33:50 +0100

    adds database configuration for applicationuser

---
+++ backend/backend/backend/src/Domain/Entities/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using backend.Domain.Enums;
+
+namespace backend.Domain.Entities;
+public class ApplicationUser
+{
+    public int Id { get; set; }
+    public string Email { get; set; }
+    public Role Role { get; set; }
+}
---
+++ backend/backend/backend/src/Infrastructure/Persistence/Configurations/ApplicationUserConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using backend.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace backend.Infrastructure.Persistence.Configurations;
+public class ApplicationUserConfiguration : IEntityTypeConfiguration<ApplicationUser>
+{
+    public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+    {
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Email).IsRequired().HasMaxLength(100);
+        builder.HasAlternateKey(x => x.Email);
+        builder.Property(x=>x.Role).IsRequired();
+    }
+}
