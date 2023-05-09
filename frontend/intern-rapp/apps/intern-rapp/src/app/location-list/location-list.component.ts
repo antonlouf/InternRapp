741ba49fcfd7f8f4699e9e4a4caafdecc84ddd1d
Recep Gultekin <Recep.Gultekin@inetum-realdolmen.world>
2023-05-09 13:57:39 +0200

    adds checkboxes for locations list

--- frontend/intern-rapp/apps/intern-rapp/src/app/location-list/location-list.component.ts
+++ frontend/intern-rapp/apps/intern-rapp/src/app/location-list/location-list.component.ts
@@ -18,11 +18,22 @@ import { MatDividerModule } from '@angular/material/divider';
 import { MatListModule } from '@angular/material/list';
 import { TranslateModule } from '@ngx-translate/core';
 import { ItemsTmplDirective } from '../items-tmpl.directive';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @Component({
   selector: 'intern-rapp-location-list',
   standalone: true,
-  imports: [CommonModule, BaselistComponent,MatIconModule, MatDividerModule, MatListModule, MatDialogModule, TranslateModule, ItemsTmplDirective],
+  imports: [
+    CommonModule,
+    BaselistComponent,
+    MatIconModule,
+    MatDividerModule,
+    MatListModule,
+    MatDialogModule,
+    TranslateModule,
+    ItemsTmplDirective,
+    MatCheckboxModule,
+  ],
   templateUrl: './location-list.component.html',
   styleUrls: ['./location-list.component.scss'],
 })
@@ -30,92 +41,132 @@ export class LocationListComponent extends BaseList<LocationItem> {
   public deleteSubject = new Subject<number>();
   public addSubject = new Subject<CreateLocation | undefined>();
   public updateSubject = new Subject<LocationItem>();
-
-  constructor(private LocationService: LocationService, public dialog: MatDialog){
-    super()
+  private selectedIds: number[] = [];
+  constructor(
+    private LocationService: LocationService,
+    public dialog: MatDialog
+  ) {
+    super();
   }
 
-  private  popUpConfig={
+  private popUpConfig = {
     width: '400px',
-   closeOnNavigation:true,
-   disableClose:false,
-   hasBackdrop:true,
-   position:{top:'150px',right:'1200px'}
-  }
+    closeOnNavigation: true,
+    disableClose: false,
+    hasBackdrop: true,
+    position: { top: '150px', right: '1200px' },
+  };
 
-  //Locations ophalen met filter 
-  protected getGridItems$(paginationFilterRequest: PaginationFilterRequest): Observable<ResourceItemPagingResponse<LocationItem>> {
-    return this.LocationService.filterAndPaginateLocations$(paginationFilterRequest)
+  //Locations ophalen met filter
+  protected getGridItems$(
+    paginationFilterRequest: PaginationFilterRequest
+  ): Observable<ResourceItemPagingResponse<LocationItem>> {
+    return this.LocationService.filterAndPaginateLocations$(
+      paginationFilterRequest
+    );
   }
 
   ngOnInit(): void {
-    this.filters=[{label:"location name",name:"filterValue",type:FilterType.Text,observable:undefined,optionBuilder:(item:unknown[])=>undefined}];
-    const delete$=this.configureDelete$();
-    const update$=this.configureUpdate$();
-    const add$=this.configureAdd$();
-    this.configureItems([delete$,update$,add$])
+    this.filters = [
+      {
+        label: 'locationCityLabel',
+        name: 'filterValue',
+        type: FilterType.Text,
+        observable: undefined,
+        optionBuilder: (item: unknown[]) => undefined,
+      },
+    ];
+    const delete$ = this.configureDelete$();
+    const update$ = this.configureUpdate$();
+    const add$ = this.configureAdd$();
+    this.configureItems([delete$, update$, add$]);
   }
-  
+
   private configureAdd$() {
-    return this.addSubject.pipe(switchMap(data => 
-      {
-      const dialogRef= this.dialog.open(LocationAddPopupComponent, this.popUpConfig) 
-      return dialogRef.afterClosed().pipe(map(confirm=> confirm!==undefined ? confirm : undefined
-         )
-    )})
-    ,filter(id => !!id)
-    ,switchMap(locItem => 
-      {
-      return  this.LocationService.addLocation$(locItem)
-      }))
-    }
+    return this.addSubject.pipe(
+      switchMap((data) => {
+        const dialogRef = this.dialog.open(
+          LocationAddPopupComponent,
+          this.popUpConfig
+        );
+        return dialogRef
+          .afterClosed()
+          .pipe(
+            map((confirm) => (confirm !== undefined ? confirm : undefined))
+          );
+      }),
+      filter((id) => !!id),
+      switchMap((locItem) => {
+        return this.LocationService.addLocation$(locItem);
+      })
+    );
+  }
 
   private configureUpdate$() {
-    return this.updateSubject.pipe(switchMap(data => 
-      {
-       
-      const dialogRef= this.dialog.open(LocationUpdatePopupComponent, this.popUpConfig) 
-      dialogRef.componentInstance.data=data
-      return dialogRef.afterClosed().pipe(map(confirm=>confirm ? data : undefined )
-    )})
-    ,filter(id => !!id) //Is Id weldegelijk een bekend Id
-    ,switchMap(locationItem=>{ // nadien geupdate ingevulde data in uit form doorsturen naar service handler 
-      console.log(locationItem)
-      return this.LocationService.updateLocation$(locationItem)
-      }))
+    return this.updateSubject.pipe(
+      switchMap((data) => {
+        const dialogRef = this.dialog.open(
+          LocationUpdatePopupComponent,
+          this.popUpConfig
+        );
+        dialogRef.componentInstance.data = data;
+        return dialogRef
+          .afterClosed()
+          .pipe(map((confirm) => (confirm ? data : undefined)));
+      }),
+      filter((id) => !!id), //Is Id weldegelijk een bekend Id
+      switchMap((locationItem) => {
+        // nadien geupdate ingevulde data in uit form doorsturen naar service handler
+        console.log(locationItem);
+        return this.LocationService.updateLocation$(locationItem);
+      })
+    );
   }
 
   private configureDelete$() {
     //return/stuur de observable verder als combinatie van filter en switchmap operaties?
-    return this.deleteSubject.pipe(switchMap(id => 
-      {
+    return this.deleteSubject.pipe(
+      switchMap((id) => {
         //open popup en geef id van te verwijderen locatie door
-      const dialogRef= this.dialog.open(DeletePopupComponent, this.popUpConfig) 
-      dialogRef.componentInstance.title="location" //why?
-      return dialogRef.afterClosed().pipe(map(confirm=>confirm ? id : undefined )
-    )})
-    ,filter(id => !!id)
-    ,switchMap(id =>  this.LocationService.deleteLocation$(id)))
+        const dialogRef = this.dialog.open(
+          DeletePopupComponent,
+          this.popUpConfig
+        );
+        dialogRef.componentInstance.title = 'location'; //why?
+        return dialogRef
+          .afterClosed()
+          .pipe(map((confirm) => (confirm ? id : undefined)));
+      }),
+      filter((id) => !!id),
+      switchMap((id) => this.LocationService.deleteLocation$(id))
+    );
   }
 
-   //filter nog niet begrepen----
-  filterUpdating(filter: {}){
-    const record = filter as Record<string, never>
+  //filter nog niet begrepen----
+  filterUpdating(filter: {}) {
+    const record = filter as Record<string, never>;
     const activeFilters: Record<string, unknown> = {};
-     this.filters?.forEach(x=>{
-       activeFilters['city']=`${record[x.name]}`
-     })
-     this.filterUpdated(activeFilters)
+    this.filters?.forEach((x) => {
+      activeFilters['city'] = `${record[x.name]}`;
+    });
+    this.filterUpdated(activeFilters);
   }
 
   //Wanneer geklikt wordt op addLocation dan worden de gepredefinieerde observers uitgevoerd
-  addLocation(){
-    this.addSubject.next(undefined)
-    }
-  updateLocation=(item: LocationItem)=>{
-  this.updateSubject.next(item)
+  addLocation() {
+    this.addSubject.next(undefined);
   }
-  delete(id: number){ 
-     this.deleteSubject.next(id)
+  updateLocation = (item: LocationItem) => {
+    this.updateSubject.next(item);
+  };
+  delete(id: number) {
+    this.deleteSubject.next(id);
+  }
+  public addToSelectedLocations(completed: boolean, id: number) {
+    if (!completed) {
+      this.selectedIds = this.selectedIds.filter((x) => x !== id);
+      return;
+    }
+    this.selectedIds.push(id);
   }
 }
